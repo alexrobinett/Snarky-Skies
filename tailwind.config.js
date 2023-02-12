11a9718d56f67696de9f773cfd8125cf954f648d
robinetta <alxr19@gmail.com>
2023-02-12 12:55:50 -0500

    theme update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,31 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+        "primary": "#0145A3",
+        
+        "secondary": "#007AFD",
+        
+        "accent": "#7A01FD",
+        
+        "neutral": "#fff",
+        
+        "base-100": "#fff",
+        
+        "info": "#5D74DA",
+        
+        "success": "#21cd8e",
+        
+        "warning": "#F9D600",
+        
+        "error": "#E06252",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
